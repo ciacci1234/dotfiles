14ef29f3b3eac1a7d435bb8a0886f5a72f596fdf
Anthony Ciacci <aciacci@bdtrust.org>
2021-03-25 11:59:15 -0400

    emacs stuff for M1 macs

--- zsh/configs/editor.zsh
+++ zsh/configs/editor.zsh
@@ -1,2 +1,4 @@
-export VISUAL=emacs
+# arch silliness happening with emacs
+# for macOS Big Sur Version 11.2 (M1 Mac, 2020)
+export VISUAL="env /usr/bin/arch -x86_64 emacs -nw"
 export EDITOR=$VISUAL
